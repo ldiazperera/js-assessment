6f3f857540813b9dc20320e1ec16ca1b6344de21
Lucas Diaz Perera <lucas.x.diaz.-nd@disney.com>
2020-01-21 17:25:12 -0300

    Async completed

--- app/async.js
+++ app/async.js
@@ -2,10 +2,28 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
+    let deferred = $.Deferred();
 
+    setTimeout(function() {
+      deferred.resolve(value);
+    }, 1);
+
+    return deferred.promise();
   },
 
   manipulateRemoteData: function(url) {
+    let deferred = $.Deferred();
 
+    $.ajax({
+      url: url
+    })
+    .then(function(data) {
+      let stringArray = $.map(data.people, function(person) {
+        return person.name;
+      });
+      deferred.resolve(stringArray.sort());
+    });
+
+    return deferred.promise();
   }
 };
