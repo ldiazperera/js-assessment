6f199e4e39cb157078b453a037663e7eaf91f308
Lucas Diaz Perera <lucas.x.diaz.-nd@disney.com>
2020-01-23 11:32:29 -0300

    Counter completed

--- app/count.js
+++ app/count.js
@@ -2,6 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    let timeout;
+    function printer () {
+      console.log(start++);
 
+      if (start <= end) {
+        timeout = setTimeout(printer, 100);
+      }
+    }
+
+    printer();
+
+    return {
+      cancel: function () {
+        timeout && clearTimeout(timeout);
+      }
+    };
   }
-};
+}
